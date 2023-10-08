b6c5810beb24d3f0d6636a2aa67319894454b5a9
Kubuś <szwaj.jakub@gmail.com>
2023-10-08 16:05:32 +0200

    ver 1

--- src/LabGit1.java
+++ src/LabGit1.java
@@ -1,7 +1,8 @@
 public class LabGit1 {
     public static void main(String[] args) {
         for(int i = 0; i<5;i++){
-            System.out.println("nr. " + i);
+            System.out.println("nr. " + i );
+
         }
     }
 }
